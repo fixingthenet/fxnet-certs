eba6bcc91b7d9a9c92bb98f28d2144ef91f16023
Peter Schrammel <peter.schrammel@gmx.de>
2018-08-21 15:43:31 +0200

    should work

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM ruby:2.4.1
+
+RUN apt-get update -y && \
+    apt-get install -y \
+            gnupg2 \
+            git-core \
+            joe \
+            curl  
+
+ENV APP_DIR=/code
+WORKDIR $APP_DIR
+
+RUN git clone https://github.com/Neilpang/acme.sh.git && \
+    cd ./acme.sh && \
+    ./acme.sh --install --force
+
+
+ADD adds/issue.rb /code/issue.rb
+
+CMD "/bin/bash"
---
+++ adds/issue.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/env ruby
+
+# currently supporting only one cert for multiple domains
+# envs:
+# DNS_PROVIDER
+# https://github.com/Neilpang/acme.sh/blob/master/dnsapi/README.md
+
+#file looks like:
+# { domains: [
+#     { fqdn: "www.example.com" }
+#   ]
+# }
+
+require 'json'
+ACME="/root/.acme.sh/acme.sh"
+
+def run(cmd)
+  puts "running: #{cmd}"
+  system(cmd)
+end
+  
+def issue
+  domains=JSON.parse(File.read("/data/domains.json"))
+  domain_names=domains["domains"].map do |domain| "-d #{domain["fqdn"]}" end.join(" ")
+  cmd=[ACME,
+  "--issue",
+  " --dns #{ENV["DNS_PROVIDER"]}",
+  domain_names,
+  "--cert-home  /data/certs",
+  "--config-home /data/config",
+  "--fullchain-file /data/fullchain.pem"
+  ].flatten.join(' ')
+  run(cmd)
+end
+
+def renew
+  cmd="#{ACME} --cron"
+  run(cmd)
+end
+
+case ARGV[0]
+when 'issue'
+  issue
+when 'renew'
+  unless File.exist?('/data/certs')
+    issue
+  end  
+  renew
+else
+  STDERR.puts("error: unknown command '#{ARGV[0]}'")
+  exit 1
+end
+
+  
